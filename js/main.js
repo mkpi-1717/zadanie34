a9dd448ec749925f0f2dfc6e91ce03e0ca8fa00b
mkpi-1717 <marekpisarczyk22@gmail.com>
2020-08-28 17:16:14 +0200

    korekta, deklaracja zmiennej, zmiana nazwy parametrów funkcji

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 let a = "Uwielbiam JavaScript"
 let b = "Jestem świetnym programistą"
 
-function c(a, b){
-    if(a.length > b.length){
-        wynik = a;
+function myFunc(num1, num2){
+    let wynik;
+    if(num1.length > num2.length){
+        wynik = num1;
     } else {
-        wynik = b;
+        wynik = num2;
     }
 return wynik;
 }
 
-console.log(c(a, b));
+console.log(myFunc(a, b));
